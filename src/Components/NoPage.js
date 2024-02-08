9b94447492cc5be3c36d3c11497d0606f967919b
superb-shan <shankar.k2021csec@sece.ac.in>
2024-02-08 22:14:33 +0530

    sonar

--- src/Components/NoPage.js
+++ src/Components/NoPage.js
@@ -3,7 +3,7 @@ export default function NoPage() {
         <div className="w-full h-screen flex justify-center items-center">
             <div className="w-[500px] h-[500px]">
                 <div style={{width:'100%', height:0, paddingBottom:'100%', position: 'relative'}}>
-                    <iframe src="https://giphy.com/embed/9J7tdYltWyXIY" title="gif" width="100%" height="100%" style={{position: 'absolute'}} frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                    <iframe src="https://giphy.com/embed/9J7tdYltWyXIY" title="gif" width="100%" height="100%" style={{position: 'absolute'}} frameBorder="0" class="giphy-embed" allowFullScreen></iframe> 
                 </div>
             </div>
         </div>
